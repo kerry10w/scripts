a9f6404ae185ca9a84551846ec4880818f704417
Kerry Cook <KCook@CoreDial.com>
2016-09-02 12:15:25 -0400

    Update to script clean up a few errors and added sudo privledges

--- Documents/svc-vm-mntr.sh
+++ Documents/svc-vm-mntr.sh
@@ -1,32 +1,19 @@
 #!/bin/sh
 
-ps auxw | grep docker | grep -v grep > /dev/null
-#ps auxw | grep service-voicemail-monitor | grep -v grep > /dev/null
+ps auxw | grep voicemail.asterisk_mailbox.daemon | grep -v grep > /dev/null
 
-if [ $? != 0 ]
+if [ $? -ne 0 ]
 then
-        #/sbin/service service-voicemail-monitor restart > /dev/null 2>&1
-       systemctl start docker 
-       echo "I should not run since service is already running"
+       sudo -i 
+       /sbin/service service-voicemail-monitor start > /dev/null 2>&1
+       exit
+else
+       sudo -i
+       svc_pid=$(pidof -s /srv/service-voicemail/bin/python -m voicemail.asterisk_mailbox_daemon)
+       threadct=$(ps hH p $svc_pid | wc -l)
+       if [ $threadct -lt 5 ]
+       then
+        /sbin/service service-voicemail-monitor restart > /dev/null 2>&1
+       fi
+       exit
 fi
-
-echo "passed docker start test"
-
-#svc_pid=$(pidof -s /srv/service-voicemail/bin/python -m voicemail.asterisk_mailbox_daemon)
-
-svc_pid=$(pidof -s /usr/bin/dockerd)
-
-echo "Service PID: " $svc_pid
-
-threadct=$(ps hH p $svc_pid | wc -l)
-
-echo "Thread Count: " $threadct
-
-if [ $threadct -lt 20 ]
-then
-        #/sbin/service service-voicemail-monitor restart > /dev/null 2>&1
-       systemctl restart docker 
-       echo "Docker has been restarted"
-fi
-
-echo "The End"
